db51b8e9dce3ae6670c43e3c90b84c80fd2c0c34
kferg357 <kennethferguson90@gmail.com>
2022-02-13 17:50:37 -0500

    need to add database

--- login.js
+++ login.js
@@ -23,3 +23,26 @@ connection.connect(function(error){
 app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
 })
+
+app.post("/",encoder, function(req,res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  connection.query("select * from loginuser where user_name = ? and user_pass = ?",[username,password],function(error,results,fields){
+      if (results.length > 0) {
+          res.redirect("/welcome");
+      } else {
+          res.redirect("/");
+      }
+      res.end();
+  })
+})
+
+// when login is success
+app.get("/welcome",function(req,res){
+  res.sendFile(__dirname + "/welcome.html")
+})
+
+
+// set app port 
+app.listen(4500);
